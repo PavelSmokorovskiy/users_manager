889c7639e55dab97e8fea01f8ead6680db17f760
Pavel <pavvel@gmail.com>
2018-05-07 08:27:48 +0300

    Initial commit

---
+++ src/main/java/task/UsersManagerApplication.java
@@ -0,0 +1,12 @@
+package task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersManagerApplication.class, args);
+    }
+}
